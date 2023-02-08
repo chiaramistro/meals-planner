41ef65d8dca105092b0e0aff2da629644a054024
Chiara Mistrorigo <chiaramistro@gmail.com>
2023-02-08 15:15:17 +0100

    [ TRANSLATION ]: add translations with i18next

--- src/index.js
+++ src/index.js
@@ -3,11 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import common_it from "./translations/it/common.json";
+import common_en from "./translations/en/common.json";
+
+i18next.init({
+  interpolation: { 
+    escapeValue: false,
+    },  // React already does escaping
+    lng: 'it',                              // language to use
+    resources: {
+        en: {
+            common: common_en               // 'common' is our custom namespace
+        },
+        it: {
+            common: common_it
+        },
+    },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
