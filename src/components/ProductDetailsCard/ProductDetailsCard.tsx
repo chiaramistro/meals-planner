5b24b4db2cb3ccde142209b9166810d00b5de736
Chiara Mistrorigo <chiaramistro@gmail.com>
2023-02-13 16:11:42 +0100

    [ PRODUCT DETAILS ]: improve gui for product details

--- src/components/ProductDetailsCard/ProductDetailsCard.tsx
+++ src/components/ProductDetailsCard/ProductDetailsCard.tsx
@@ -1,11 +1,31 @@
 import { Product } from "../../models/Product";
 import "./ProductDetailsCard.css";
+import barcode from '../../assets/barcode.svg'
+import { useTranslation } from "react-i18next";
 
 const ProductDetailsCard = (props: {product: Product}) => {
   const { product } = props;
+  const [t] = useTranslation('common');
 
   return (
-    <p>Product card {product.name}</p>
+    <div className="card">
+      <div className="ProductDetailsCard-Info">
+
+      <div className="ProductDetailsCard-Info-Main">
+      <span className="ProductDetailsCard-Info-Main-Name">{product.name}</span>
+      <span className="ProductDetailsCard-Info-Main-Category">{product.category}</span>
+      </div>
+
+      <div className="ProductDetailsCard-Info-Price">
+        <p className="ProductDetailsCard-Info-Price-OutOfStock">{product.quantity <= 0 ? t('product.details.outOfStock') : ""}</p>
+        <span className="ProductDetailsCard-Info-Price-Text">{product.price}</span>
+      </div>
+      </div>
+
+      <div className="ProductDetailsCard-Barcode">
+      <img className="ProductDetailsCard-Barcode-Img" src={barcode} alt="Product barcode" />
+      </div>
+    </div>
   );
 };
 
--- src/screens/ProductDetailsScreen/ProductDetailsScreen.tsx
+++ src/screens/ProductDetailsScreen/ProductDetailsScreen.tsx
@@ -6,6 +6,34 @@ import { useParams } from "react-router-dom";
 import ProductDetailsCard from '../../components/ProductDetailsCard/ProductDetailsCard';
 import PageTitle from '../../components/common/PageTitle/PageTitle';
 
+const ProductDetails = (props: {product: Product}) => {
+  const { product } = props;
+  const [t] = useTranslation('common');
+
+  return (
+    <div className="ProductDetails">
+      <PageTitle title={t('product.details.title')} />
+      <ProductDetailsCard product={product}/>
+    </div>
+  );
+}
+
+const NoProductFound = (props: {name: string}) => {
+  const { name } = props;
+  const [t] = useTranslation('common');
+
+  return (
+    <div className="ProductDetails">
+      <PageTitle title={t('attention')} />
+      <div className="card">
+        <div className="NoProductFound-Text">
+          <span>{t('product.details.notFound', {prodName: name})}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function ProductDetailsScreen() {
     const [t] = useTranslation('common');
     let params = useParams(); // navigation params
@@ -13,16 +41,14 @@ function ProductDetailsScreen() {
 
     const index = GROCERY_LIST.findIndex((prod: Product) => prod.name.toLowerCase() === params.productName?.toLowerCase()) 
     if (index !== -1) {
-      pageContent.push(<PageTitle title={GROCERY_LIST[index].name} />)
-      pageContent.push(<ProductDetailsCard product={GROCERY_LIST[index]}/>)
+      pageContent.push(<ProductDetails product={GROCERY_LIST[index]}/>)
     } else {
-      pageContent.push(<p>No product found with name {params.productName}</p>)
+      pageContent.push(<NoProductFound name={params.productName ?? ""} />)
     }
   
     return (
       <div className="ProductDetailsScreen">
        {pageContent}
-       
       </div>
     );
   }
